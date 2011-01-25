472a50dc340e33134a328f31ae530e9e807fb58e
mattgerg <mattgerg@localhost>
2011-01-25 15:49:33 -0500

    * Added DeleteOlderThan(timeSpan) to PureImageCache interface * Implemented DeleteOlderThan(timeSpan) for SQLite cache

---
+++ GMap.NET.Core/GMap.NET/PureImageCache.cs
@@ -0,0 +1,37 @@
+﻿
+namespace GMap.NET
+{
+   using System.IO;
+
+   /// <summary>
+   /// pure abstraction for image cache
+   /// </summary>
+   public interface PureImageCache
+   {
+      /// <summary>
+      /// puts image to db
+      /// </summary>
+      /// <param name="tile"></param>
+      /// <param name="type"></param>
+      /// <param name="pos"></param>
+      /// <param name="zoom"></param>
+      /// <returns></returns>
+      bool PutImageToCache(MemoryStream tile, MapType type, GPoint pos, int zoom);
+
+      /// <summary>
+      /// gets image from db
+      /// </summary>
+      /// <param name="type"></param>
+      /// <param name="pos"></param>
+      /// <param name="zoom"></param>
+      /// <returns></returns>
+      PureImage GetImageFromCache(MapType type, GPoint pos, int zoom);
+
+      /// <summary>
+      /// delete old tiles beyond a supplied timespan
+      /// </summary>
+      /// <param name="timeSpan">Tiles older than this will be deleted.</param>
+      /// <returns>The number of deleted tiles.</returns>
+      int DeleteOlderThan(System.TimeSpan timeSpan);
+   }
+}
