3a108a052befedb051c8371ea14932cad25ca03d
aconcagua21 <aconcagua21@localhost>
2016-06-06 09:01:56 +0200

    Updated tile addressing for all Czech map providers (mapy.cz). Added 2 map providers (CzechTuristWinterMapProvider, CzechGeographicMapProvider).

---
+++ GMap.NET.Core/GMap.NET.MapProviders/Czech/CzechHistoryMapProvider.cs
@@ -0,0 +1,75 @@
+﻿
+namespace GMap.NET.MapProviders
+{
+   using System;
+
+   /// <summary>
+   /// CzechHistoryMap provider, http://www.mapy.cz/
+   /// </summary>
+   public class CzechHistoryMapProvider : CzechMapProviderBase
+   {
+      public static readonly CzechHistoryMapProvider Instance;
+
+      CzechHistoryMapProvider()
+      {
+         Area = new RectLatLng(51.2024819920053, 11.8401353319027, 7.22833716731277, 2.78312271922872);
+         MaxZoom = 15;
+      }
+
+      static CzechHistoryMapProvider()
+      {
+         Instance = new CzechHistoryMapProvider();
+      }
+
+      #region GMapProvider Members
+
+      readonly Guid id = new Guid("C666AAF4-9D27-418F-97CB-7F0D8CC44544");
+      public override Guid Id
+      {
+         get
+         {
+            return id;
+         }
+      }
+
+      readonly string name = "CzechHistoryMap";
+      public override string Name
+      {
+         get
+         {
+            return name;
+         }
+      }
+
+      GMapProvider[] overlays;
+      public override GMapProvider[] Overlays
+      {
+         get
+         {
+            if(overlays == null)
+            {
+               overlays = new GMapProvider[] { this, CzechHybridMapProvider.Instance };
+            }
+            return overlays;
+         }
+      }
+
+      public override PureImage GetTileImage(GPoint pos, int zoom)
+      {
+         string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+
+         return GetTileImageUsingHttp(url);
+      }
+
+      #endregion
+
+      string MakeTileImageUrl(GPoint pos, int zoom, string language)
+      {
+         // http://m3.mapserver.mapy.cz/army2-m/14-8802-5528
+
+         return string.Format(UrlFormat, GetServerNum(pos, 3) + 1, zoom, pos.X, pos.Y);
+      }
+
+      static readonly string UrlFormat = "http://m{0}.mapserver.mapy.cz/army2-m/{1}-{2}-{3}";
+   }
+}
---
+++ GMap.NET.Core/GMap.NET.MapProviders/Czech/CzechSatelliteMapProvider.cs
@@ -0,0 +1,60 @@
+﻿
+namespace GMap.NET.MapProviders
+{
+   using System;
+
+   /// <summary>
+   /// CzechSatelliteMap provider, http://www.mapy.cz/
+   /// </summary>
+   public class CzechSatelliteMapProvider : CzechMapProviderBase
+   {
+      public static readonly CzechSatelliteMapProvider Instance;
+
+      CzechSatelliteMapProvider()
+      {
+      }
+
+      static CzechSatelliteMapProvider()
+      {
+         Instance = new CzechSatelliteMapProvider();
+      }
+
+      #region GMapProvider Members
+
+      readonly Guid id = new Guid("7846D655-5F9C-4042-8652-60B6BF629C3C");
+      public override Guid Id
+      {
+         get
+         {
+            return id;
+         }
+      }
+
+      readonly string name = "CzechSatelliteMap";
+      public override string Name
+      {
+         get
+         {
+            return name;
+         }
+      }
+
+      public override PureImage GetTileImage(GPoint pos, int zoom)
+      {
+         string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+
+         return GetTileImageUsingHttp(url);
+      }
+
+      #endregion
+
+      string MakeTileImageUrl(GPoint pos, int zoom, string language)
+      {
+         // http://m3.mapserver.mapy.cz/ophoto-m/14-8802-5528
+
+         return string.Format(UrlFormat, GetServerNum(pos, 3) + 1, zoom, pos.X, pos.Y);
+      }
+
+      static readonly string UrlFormat = "http://m{0}.mapserver.mapy.cz/ophoto-m/{1}-{2}-{3}";
+   }
+}
